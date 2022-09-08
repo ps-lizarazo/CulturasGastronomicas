2002405fed412835c79d4d6710bf1aee0db5d864
ayrtonpastor <a.pastor@uniandes.edu.co>
2022-09-07 22:58:58 -0500

    Develop (#60)
    * ReOrder app module
    
    * feat: add service for relationship between restaurants and cultures. (#47)
    
    * Implementación de lógica y pruebas asociaciones culturaGastronomica y recetas
    
    * Controlador para categoria (#52)
    
    * Implementación de controladores de cultura gastronómica y receta (#57)
    
    * Corrección de controlador de asociación (#59)
    
    Co-authored-by: ps-lizarazo <ps.lizarazo@uniandes.edu.co>
    Co-authored-by: Miguel Camargo <ma.camargo45@uniandes.edu.co>
    Co-authored-by: Andres Benavides <f.benavidesv@uniandes.edu.co>

--- src/receta-cultura_gastronomica/receta-cultura_gastronomica.module.ts
+++ src/receta-cultura_gastronomica/receta-cultura_gastronomica.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { RecetaEntity } from '../receta/receta.entity';
 import { CulturaGastronomicaEntity } from '../cultura_gastronomica/cultura_gastronomica.entity';
-import { CulturaGastronomicaService } from '../cultura_gastronomica/cultura_gastronomica.service';
 import { RecetaCulturaGastronomicaController } from './receta-cultura_gastronomica.controller';
+import { RecetaCulturaGastronomicaService } from './receta-cultura_gastronomica.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([CulturaGastronomicaEntity])],
-    providers: [CulturaGastronomicaService],
+    imports: [TypeOrmModule.forFeature([CulturaGastronomicaEntity, RecetaEntity])],
+    providers: [RecetaCulturaGastronomicaService],
     controllers: [RecetaCulturaGastronomicaController],
 })
 export class RecetaCulturaGastronomicaModule {}
