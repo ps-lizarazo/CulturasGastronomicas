42bc3ab1cc336035d5404d9d2861b06f6c7b9057
Andres Benavides <f.benavidesv@uniandes.edu.co>
2022-09-27 18:30:02 -0500

    Agregar graphQL a las categorias (#92)

--- src/categoria/categoria.entity.ts
+++ src/categoria/categoria.entity.ts
@@ -1,14 +1,20 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { ProductoEntity } from '../producto/producto.entity';
+import { Field, ObjectType } from '@nestjs/graphql';
 
+@ObjectType()
 @Entity()
 export class CategoriaEntity {
+
+  @Field()
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Field()
   @Column()
   nombre: string;
 
+  @Field(type => [ProductoEntity])
   @OneToMany(() => ProductoEntity, (producto) => producto.categoria)
   productos: ProductoEntity[];
 }
